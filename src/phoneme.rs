2fca81cbefa4edbf0691d6c77f2d02c1e9cf85ee
Ben Spalding <bnspalding418@gmail.com>
2021-07-08 14:13:09 -0400

    symbol fn for phonemes, tests for symbols

--- src/phoneme.rs
+++ src/phoneme.rs
@@ -17,3 +17,54 @@ pub enum Phoneme {
     ///A phoneme comprised of an ordered sequence of two segments
     Disegment(Segment, Segment),
 }
+
+impl Phoneme {
+    ///The symbol associated with a phoneme. ex: 'p', 't͡ʃ'
+    pub fn symbol(&self) -> String {
+        match self {
+            Phoneme::Monosegment(s) => s.symbol.to_string(),
+            Phoneme::Disegment(s1, s2) => {
+                format!("{}͡{}", &s1.symbol, &s2.symbol)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Phoneme::{Disegment, Monosegment};
+    use super::*;
+    use crate::features::*;
+
+    #[test]
+    fn symbol_monosegment() {
+        let m = Monosegment(mk_segment("p"));
+        assert_eq!(m.symbol(), "p")
+    }
+
+    #[test]
+    fn symbol_disegment() {
+        let m = Disegment(mk_segment("t"), mk_segment("ʃ"));
+        assert_eq!(m.symbol(), "t͡ʃ")
+    }
+
+    fn mk_segment(sym: &str) -> Segment {
+        Segment {
+            root_features: RootFeatures {
+                consonantal: BinaryFeature::Minus,
+                sonorant: BinaryFeature::Minus,
+                syllabic: BinaryFeature::Minus,
+            },
+            autosegmental_features: AutosegmentalFeatures {
+                nasal: None,
+                lateral: None,
+                rhotic: None,
+                strident: None,
+                continuant: None,
+                place: None,
+                laryngeal: None,
+            },
+            symbol: sym.to_string(),
+        }
+    }
+}
