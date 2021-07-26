1e657d7f8fbfca70064914f071976e86306552a8
Ben Spalding <bnspalding418@gmail.com>
2021-07-26 13:53:20 -0400

    add stress and syllable structs

---
+++ src/stress.rs
@@ -0,0 +1,51 @@
+//! Levels of lexical stress for syllables
+//!
+//! Stress provides four levels of marked stress for a syllable. The four
+//! levels of stress can also reduce to either high or low stress for scenarios
+//! where a binary measure of stress is more appropriate.
+//!
+//! There doesn't appear to be solid agreement about how many levels of
+//! distinguishable stress are useful, so the choice to use four (with means
+//! for reducing to two) is motivated by leaving the greatest number of options
+//! open. The CMU Pronouncing Dictionary uses three levels, which would map to
+//! 0-Unstressed, 1-Stressed 2-SecondaryStress.
+
+/// Stress is represented with four levels of emphasis. Use [`toBinaryStress`]
+/// to reduce these four levels to binary stress.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum Stress {
+    /// The syllable is not only least emphasized, but also reduced
+    ReducedStress,
+    /// The syllable is less emphasized than surrounding syllables
+    Unstressed,
+    /// The syllable has stress, but is not the most prominent in a word
+    SecondaryStress,
+    /// The syllable is more emphasized than surrounding syllables
+    Stressed,
+}
+
+impl Stress {
+    /// to_binary_stress converts four-level stress to two-level stress.
+    ///
+    /// ReducedStress and Unstressed become Unstressed,
+    /// SecondaryStress and Stressed become Stressed.
+    pub fn to_binary_stress(&self) -> BinaryStress {
+        match self {
+            Stress::ReducedStress | Stress::Unstressed => {
+                BinaryStress::Unstressed
+            }
+            Stress::SecondaryStress | Stress::Stressed => {
+                BinaryStress::Stressed
+            }
+        }
+    }
+}
+
+/// BinaryStress represents two levels of syllable emphasis
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum BinaryStress {
+    /// The syllable is less emphasized than surrounding syllables
+    Unstressed,
+    /// The syllable is more emphasized than surrounding syllables
+    Stressed,
+}
---
+++ src/syllable.rs
@@ -0,0 +1,96 @@
+//! Structured collection of phonemes.
+//!
+//! Syllables are broken into three groups of phonemes: onset, nucleus, and
+//! coda. The nucleus and the coda together are commonly known as the rhyme.
+//! A syllable is also marked with a level of stress, although this really only
+//! takes on meaning in relation to other syllables in the same word (lexical
+//! stress).
+
+use crate::phoneme::Phoneme;
+use crate::stress::Stress;
+
+/// Syl describes a structured collection of phonemes, what people commonly
+/// distinguish as the unit out of which words are constructed.
+#[derive(PartialEq, Eq, Debug)]
+pub struct Syl {
+    /// The onset is the collection of phonemes that begin a syllable
+    pub onset: Vec<Phoneme>,
+    /// The nucleus, normally a vowel, is the most sonorous phoneme in the syl.
+    pub nucleus: Phoneme,
+    /// The coda is the collection of phonemes that follow the nucleus.
+    pub coda: Vec<Phoneme>,
+    /// A syllable's stress marks the level of stress that is put on the syllable.
+    /// It is only meaningful in relation to surrounding syllables in the same word.
+    pub stress: Option<Stress>,
+}
+
+impl Syl {
+    /// The rhyme is the nucleus and coda of a syllable together.
+    pub fn rhyme(&self) -> Vec<Phoneme> {
+        let mut vec = vec![self.nucleus];
+        vec.extend(&self.coda);
+        vec
+    }
+
+    /// phonemes flattens a syllable into a single ordered list of phonemes.
+    pub fn phonemes(&self) -> Vec<Phoneme> {
+        let mut vec = Vec::new();
+        vec.extend(&self.onset);
+        vec.push(self.nucleus);
+        vec.extend(&self.coda);
+        vec
+    }
+
+    /// symbols returns the symbolic representation of a syllable's phonemes as a
+    /// single String.
+    ///
+    /// Because stress is only relevant between syllables, it is not rendered
+    /// as part of a Syl's symbols, but instead as part of a Word's symbols.
+    pub fn symbols(&self) -> String {
+        self.phonemes()
+            .iter()
+            .fold(String::new(), |acc, p| acc + &p.symbol())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::accents::genam::phoneme;
+    use crate::phoneme::Phoneme;
+    use crate::stress::Stress;
+
+    fn test_syl() -> Syl {
+        Syl {
+            onset: vec![phon("p"), phon("ɹ")],
+            nucleus: phon("ɑ"),
+            coda: vec![phon("p")],
+            stress: Some(Stress::Stressed),
+        }
+    }
+
+    fn phon(s: &str) -> Phoneme {
+        phoneme(s).unwrap()
+    }
+
+    #[test]
+    fn test_rhyme() {
+        let test_syl = test_syl();
+        assert_eq!(test_syl.rhyme(), vec![phon("ɑ"), phon("p")])
+    }
+
+    #[test]
+    fn test_phonemes() {
+        let test_syl = test_syl();
+        assert_eq!(
+            test_syl.phonemes(),
+            vec![phon("p"), phon("ɹ"), phon("ɑ"), phon("p")]
+        )
+    }
+
+    #[test]
+    fn test_symbols() {
+        let test_syl = test_syl();
+        assert_eq!(test_syl.symbols(), String::from("pɹɑp"))
+    }
+}
